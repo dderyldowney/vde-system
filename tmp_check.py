909e6758d218fee2283046bfe8e1ca0d0ba06c63
D Deryl Downey <133974812+dderyldowney@users.noreply.github.com>
2026-03-20 20:40:10 -0400

    refactor: delete temp file tmp_check.py

--- tmp_check.py
+++
@@ -1,7 +0,0 @@
-import json, shutil
-
-print("jq path:", shutil.which("jq"))
-with open("data/vm-types.json") as f:
-    data = json.load(f)
-langs = [vm["name"] for vm in data["vms"]["language"]]
-print("languages in json:", langs)
